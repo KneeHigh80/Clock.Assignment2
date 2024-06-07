a17aa6d4746bc8ab29d8cfe46fdb4de521c1f787
KneeHigh80 <lukasrichly@gmail.com>
2024-06-07 13:10:22 +0100

    Seeting up Alarm

---
+++ src/clock/Alarm.java
@@ -0,0 +1,57 @@
+package clock;
+
+
+
+public class Alarm implements Comparable<Alarm> {
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private String label;
+    
+    
+    // Constructor
+    public Alarm(int hours, int minutes, int seconds, String label) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.label = label;
+    }
+    
+    
+    //Getters and Setters
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    
+    
+    
+}
